17740388793a0a104989789134afd9e67e0da3e1
Ravi Tharun <tr565003@gmail.com>
2026-05-03 14:31:53 +0530

    added the feat mantance mode

--- Ui/src/main.tsx
+++ Ui/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import FeatMaintenance from './Components/FeatMaintance.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <FeatMaintenance>
     <App />
-  </StrictMode>,
+  </FeatMaintenance>,
 )
