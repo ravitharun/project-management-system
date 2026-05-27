a2ed7c2688d901fa74738738d5dd643526087145
Ravi Tharun <tr565003@gmail.com>
2026-05-27 16:05:29 +0530

    fix(env,apiservices) updated the env,apiserviceswith prod apilogic

--- Ui/src/services/apiservices.ts
+++ Ui/src/services/apiservices.ts
@@ -4,6 +4,8 @@ export const token = "12"
 
 
 const instanceurl = import.meta.env.Api === 'prod' ? import.meta.env.VITE_API : "http://localhost:5000/"
+const env = import.meta.env.Api
+console.log({ instanceurl, env })
 
 
 export const instance = axios.create({
