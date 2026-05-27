19fb3f6be98f39390784d072979890e765db8f0b
Ravi Tharun <tr565003@gmail.com>
2026-05-27 16:55:17 +0530

    fix

--- Ui/src/Scokets/ScoketConfig.ts
+++ Ui/src/Scokets/ScoketConfig.ts
@@ -4,7 +4,6 @@ import { getuserInfo } from "../Components/LocalStorage";
 
 export const socket = io(import.meta.env.VITE_ENV === 'prod' ? import.meta.env.VITE_API : "http://localhost:5000/", {
     auth: { userId: getuserInfo ? JSON.parse(getuserInfo).userEmail : null },
-
     reconnection: true,
 
 });
