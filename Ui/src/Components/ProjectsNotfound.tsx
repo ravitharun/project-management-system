9d9c86a335149e71a4f74f4482c3a5e5d630cca8
Ravi Tharun <tr565003@gmail.com>
2026-05-28 17:20:36 +0530

    style:Updated the Ui

--- Ui/src/Components/ProjectsNotfound.tsx
+++ Ui/src/Components/ProjectsNotfound.tsx
@@ -1,40 +1,42 @@
 import { FaProjectDiagram } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-
 type ProjectsNotFoundPropd = {
     title: string,
     message: string
+
 }
 
 function ProjectsNotfound({ title, message }: ProjectsNotFoundPropd) {
-
     return (
-        <div className="fixed inset-0 flex items-center justify-center pt-20 pl-50">
 
-            <div className="flex flex-col items-center justify-center text-center">
+
+        <>
+
+
+            <div className="mt-25 flex flex-col items-center justify-center text-center py-10">
 
                 <div className="bg-blue-100 p-4 rounded-full shadow-md">
                     <FaProjectDiagram className="text-4xl text-blue-600" />
                 </div>
 
                 <h2 className="mt-4 text-lg font-semibold text-gray-700">
+                    {/* {No Project Progress Found} */}
                     {title}
                 </h2>
-
                 <Link to="/projects">
 
                     <p
                         className="text-sm text-gray-500 mt-1 cursor-pointer transition-all duration-300 hover:text-blue-500 hover:scale-105"
                         title="Create Projects"
                     >
+                        {/* Start creating projects to track progress here. */}
                         {message}
+                        
                     </p>
-
                 </Link>
 
             </div>
-
-        </div>
+        </>
     )
 }
 
