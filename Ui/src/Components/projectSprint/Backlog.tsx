48082c3c0db29b003345883557eefef2ae8f26e1
Ravi Tharun <tr565003@gmail.com>
2026-08-05 13:49:37 +0530

    feat: add sprint management feature

---
+++ Ui/src/Components/projectSprint/Backlog.tsx
@@ -0,0 +1,18 @@
+import Loader from "../Loader"
+
+const Backlog=()=>{
+
+
+    return(
+
+
+        <>
+        Backlog
+        <Loader/>
+        
+        </>
+    )
+}
+
+
+export default Backlog
