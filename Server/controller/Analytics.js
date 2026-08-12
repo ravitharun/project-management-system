82549c7910c89269ae78c05317aea428592bdfc4
Ravi Tharun <tr565003@gmail.com>
2026-08-12 09:27:59 +0530

    Feat(Viewd): added the feat recenlty viewd workspaces tab

--- Server/controller/Analytics.js
+++ Server/controller/Analytics.js
@@ -2,17 +2,20 @@
 
 const WorkspaceSchema = require("../Models/Workspace")
 const WorkSpaceTask = require("../Models/WorkSapceTask")
+const WorkspaceViewed= require("../Models/ViwedOn")
+const User= require("../Models/Auth")
+
 const FetchView = async (req, res) => {
 
 
 
     try {
-        const { projectId,userid } = req.params
+        const { projectId, userid } = req.params
         console.log(req.params, 'projectId');
         const ProjectInfo = await WorkspaceSchema.findById({ _id: projectId }).populate('WorkSpacememebers.id')
         console.log(ProjectInfo, 'ProjectInfo');
         const ProjectTask = await WorkSpaceTask.find({ projectid: projectId })
-        const GetuseridTask=await WorkSpaceTask.find({ assignTo: userid })
+        const GetuseridTask = await WorkSpaceTask.find({ assignTo: userid })
         console.log(GetuseridTask, 'GetuseridTask');
         const tasktodo = ProjectTask.filter((task) => task.ProjectTask == "todo")
         const taskCompleted = ProjectTask.filter((task) => task.ProjectTask == "Completed")
@@ -23,7 +26,7 @@ const FetchView = async (req, res) => {
         const TaskHigh = ProjectTask.filter((task) => task.Priority == "High")
         const Summary = {
             ProjectInfo,
-            "GetuseridTask":GetuseridTask,
+            "GetuseridTask": GetuseridTask,
             "TotalTask": ProjectTask.length,
             "taskCompleted": taskCompleted.length,
             "taskInProgress": taskInProgress.length,
@@ -42,13 +45,56 @@ const FetchView = async (req, res) => {
         return res.status(200).json({ message: "Summary", data: Summary, status: true })
     } catch (error) {
 
-        console.log(error.message,'tharun');
+        console.log(error.message, 'tharun');
 
         return res.status(500).json({ message: "Server Error" })
         // return res.status(200).json({ message: "Summary", data: ProjectInfo, status: true})
 
     }
 }
+const Viewed = async (req, res) => {
+
+    try {
+
+        const { userid } = req.query
+        console.log(userid, 'prm');
 
 
-module.exports = { FetchView }
+        if (!userid) {
+
+            return res.status(400).json({ message: "userID is Missing.. " })
+
+        }
+
+
+
+
+
+
+        const isExitsUser = await User.findById({ _id: userid })
+        if (!isExitsUser) {
+            return res.status(404).json({ message: "user Is not Exits " })
+
+        }
+
+        const viwed_workspace = await WorkspaceViewed.find({UserId:userid}).populate('viewedWorkspaces.WorkspaceId')
+        console.log(viwed_workspace,'viwed_workspace');
+        
+
+
+
+
+
+
+        return res.status(200).json({ message: "", data: viwed_workspace, status: true })
+
+
+    } catch (error) {
+        console.log(error.message,'errT');
+        
+        return res.status(500).json({ message: "server error", status: false })
+
+    }
+}
+
+module.exports = { FetchView, Viewed }
