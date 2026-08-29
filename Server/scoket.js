0c0ee96255175c60af8138b562b5f62d2fdc69ae
Ravi Tharun <tr565003@gmail.com>
2026-08-29 09:48:29 +0530

    Feat: Implement version deletion with confirmation alert

--- Server/scoket.js
+++ Server/scoket.js
@@ -16,7 +16,10 @@ const initSocket = (server) => {
     cors: {
       origin: [
         "http://localhost:5173",
-        "https://taskora-system.netlify.app", "https://devserver-testing--taskora-system.netlify.app", 'https://project-management-system-weld-eight.vercel.app', 'https://project-management-system-jsxe.vercel.app'
+        "https://taskora-system.netlify.app", 
+        "https://devserver-testing--taskora-system.netlify.app", 
+        'https://project-management-system-weld-eight.vercel.app', 
+        'https://project-management-system-jsxe.vercel.app'
       ],
     },
   });
