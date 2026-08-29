8bc5d7a9cf48a561a6b7894eaa19043eafc0b4d9
Ravi Tharun <tr565003@gmail.com>
2026-08-29 09:59:59 +0530

    Feat: Implement version deletion with confirmation alert

--- Ui/src/Scokets/ScoketConfig.ts
+++ Ui/src/Scokets/ScoketConfig.ts
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 import { getuserInfo } from "../Components/LocalStorage";
 
-export const socket = io(import.meta.env.VITE_Prod=="Local"? "http://localhost:5000":'https://project-management-system-jsxe.vercel.app', {
+export const socket = io(import.meta.env.VITE_Prod=="Local"? "http://localhost:5000":import.meta.env.VITE_API, {
   auth: { userId: getuserInfo ? JSON.parse(getuserInfo).userEmail : null },
   reconnection: true,
 });
